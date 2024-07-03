843581fd9ac119b42122d4487ecdc00d9241bf23
neeldholiya04 <neeldholiya04@gmail.com>
2024-07-03 10:35:07 +0530

    added put functionality

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 let courese = [
     {id: 1, name: "John", email: "john@example.com"},
     {id: 2, name: "John", email: "john@example.com"},
@@ -14,12 +16,23 @@ app.get('/', (req, res) => {
 );
 
 app.post('/', (req, res) => {
-    const course = {
+    let singleCourse = {
         id: courese.length + 1,
-        name: req.body.name
-    };
-    courese.push(course);
+        name: req.body.name,
+        email: req.body.email
+    }
+    courese.push(singleCourse);
+    res.json(courese);
+}
+);
+
+app.put('/:id', (req, res) => {
+    let course = courese.find(c => c.id === parseInt(req.params.id));
+
+    course.name = req.body.name;
+    course.email = req.body.email;
     res.json(course);
+
 }
 );
 
